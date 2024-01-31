4f4abd148c4ceb021a7f453d7b50b1ac197e8189
soebedjho <gerosellysas@gmail.com>
2024-01-31 17:16:39 +0700

    add: screen_util lib

--- lib/infrastructure/sources/constants/fonts.constant.dart
+++ lib/infrastructure/sources/constants/fonts.constant.dart
@@ -1,31 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:test_pt_seru/infrastructure/sources/constants/hues.constant.dart';
 
 class Fonts {
   static TextStyle normal({Color? color, double? size}) => TextStyle(
         color: color ?? Hues.black,
-        fontSize: size ?? 14,
+        fontSize: size ?? 14.sp,
         fontWeight: FontWeight.w400,
         fontFamily: "Inter",
       );
 
   static TextStyle semiBold({Color? color, double? size}) => TextStyle(
         color: color ?? Hues.black,
-        fontSize: size ?? 14,
+        fontSize: size ?? 14.sp,
         fontWeight: FontWeight.w600,
         fontFamily: "Inter",
       );
 
   static TextStyle bold({Color? color, double? size}) => TextStyle(
         color: color ?? Hues.black,
-        fontSize: size ?? 14,
+        fontSize: size ?? 14.sp,
         fontWeight: FontWeight.w700,
         fontFamily: "Inter",
       );
 
   static TextStyle italic({Color? color, double? size}) => TextStyle(
         color: color ?? Hues.black,
-        fontSize: size ?? 14,
+        fontSize: size ?? 14.sp,
         fontWeight: FontWeight.w400,
         fontStyle: FontStyle.italic,
         fontFamily: "Inter",
@@ -33,7 +34,7 @@ class Fonts {
 
   static TextStyle semiBoldItalic({Color? color, double? size}) => TextStyle(
         color: color ?? Hues.black,
-        fontSize: size ?? 14,
+        fontSize: size ?? 14.sp,
         fontWeight: FontWeight.w600,
         fontStyle: FontStyle.italic,
         fontFamily: "Inter",
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
 
 import 'infrastructure/navigation/navigation.dart';
 import 'infrastructure/navigation/routes.dart';
+import 'infrastructure/sources/constants/constants.dart';
 
 void main() async {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   var initialRoute = await Routes.initialRoute;
   runApp(Main(initialRoute));
 }
@@ -16,9 +20,22 @@ class Main extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      initialRoute: initialRoute,
-      getPages: Nav.routes,
+    return ScreenUtilInit(
+      designSize: const Size(400, 800),
+      minTextAdapt: true,
+      splitScreenMode: false,
+      child: MediaQuery(
+        data: const MediaQueryData(textScaler: TextScaler.linear(1.0)),
+        child: GetMaterialApp(
+          initialRoute: initialRoute,
+          getPages: Nav.routes,
+          theme: ThemeData(
+            colorSchemeSeed: Hues.primary,
+            fontFamily: "Inter",
+            useMaterial3: true,
+          ),
+        ),
+      ),
     );
   }
 }
