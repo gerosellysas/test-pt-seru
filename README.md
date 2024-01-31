061c62df4e937e4c22f4b8aafd3fb8ac3b7122a2
soebedjho <gerosellysas@gmail.com>
2024-01-31 13:45:46 +0700

    first initialize

---
+++ README.md
@@ -0,0 +1,5 @@
+# Test PT SERU
+
+Application for recruitment process held by PT. Solusi Eksplorasi Rembulan Utama.
+
+# Features
---
+++ android/app/src/main/kotlin/com/example/test_pt_seru/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.test_pt_seru
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
