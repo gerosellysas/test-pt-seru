94cf5940b4034d3132bca8d0e4bc6a5881465ab8
soebedjho <gerosellysas@gmail.com>
2024-02-01 13:53:56 +0700

    handle network check availability method

--- lib/presentation/components/services/app.service.dart
+++ lib/presentation/components/services/app.service.dart
@@ -1,23 +1,40 @@
+import 'dart:async';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:test_pt_seru/feature/connection/connection.dart';
 
 class AppService extends GetxService with WidgetsBindingObserver {
+  final NetworkCheckAvailabilityUseCase _networkCheckAvailability =
+      NetworkCheckAvailabilityUseCase();
+
   final _view = WidgetsBinding.instance.platformDispatcher.views.first;
 
-  var bottomHeight = 0.0.obs;
+  StreamSubscription<ConnectivityResult>? streamConnectivityResult;
 
+  var connectivity = ConnectivityResult.none.obs;
   var personal = ["".obs, "".obs, "".obs];
   var region = ["".obs, "".obs, "".obs, "".obs];
   var cardId = "".obs;
+  var bottomHeight = 0.0.obs;
+
+  @override
+  void onInit() {
+    WidgetsBinding.instance.addObserver(this);
+    super.onInit();
+  }
 
   @override
   void onReady() {
-    WidgetsBinding.instance.addObserver(this);
+    startCheckingConnectivity();
+
     super.onReady();
   }
 
   @override
   void onClose() {
+    stopCheckingConnectivity();
     WidgetsBinding.instance.removeObserver(this);
     super.onClose();
   }
@@ -26,5 +43,19 @@ class AppService extends GetxService with WidgetsBindingObserver {
   void didChangeMetrics() {
     bottomHeight.value = _view.viewInsets.bottom / _view.devicePixelRatio;
     super.didChangeMetrics();
+  }
+
+  Future<void> startCheckingConnectivity() async {
+    await stopCheckingConnectivity();
+    streamConnectivityResult =
+        _networkCheckAvailability.execute().listen((result) {
+      connectivity.value = result;
+    });
+  }
+
+  Future<void> stopCheckingConnectivity() async {
+    if (streamConnectivityResult == null) return;
+    streamConnectivityResult?.cancel();
+    streamConnectivityResult = null;
   }
 }
