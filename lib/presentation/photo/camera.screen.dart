168c0db9117a6be32692487da29cbcd33d308501
soebedjho <gerosellysas@gmail.com>
2024-02-02 00:23:30 +0700

    create: different screen for front & back camera

--- lib/presentation/photo/camera.screen.dart
+++ lib/presentation/photo/camera.screen.dart
@@ -26,6 +26,103 @@ class CameraScreen extends StatelessWidget {
         ),
       );
     }
+    if (controller!.description.lensDirection == CameraLensDirection.front) {
+      return _CameraFront(
+        key: key,
+        controller: controller,
+        onShutter: onShutter,
+      );
+    }
+    return _CameraBack(
+      key: key,
+      controller: controller!,
+      onShutter: onShutter,
+    );
+  }
+}
+
+class _CameraFront extends StatelessWidget {
+  final CameraController? controller;
+  final void Function()? onShutter;
+
+  const _CameraFront({
+    super.key,
+    this.controller,
+    this.onShutter,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(height: ScreenUtil().statusBarHeight + 8.h),
+        Row(
+          children: [
+            SizedBox(width: 24.w),
+            _CameraFlash(
+              key: key,
+              icon: Images.flashOff,
+              onTap: () => controller!.setFlashMode(FlashMode.off),
+            ),
+            const Expanded(child: SizedBox()),
+            SizedBox(width: 24.w),
+            _CameraFlash(
+              key: key,
+              icon: Images.flashOn,
+              onTap: () => controller!.setFlashMode(FlashMode.torch),
+            ),
+            const Expanded(child: SizedBox()),
+            SizedBox(width: 24.w),
+            _CameraFlash(
+              key: key,
+              icon: Images.flashAuto,
+              onTap: () => controller!.setFlashMode(FlashMode.auto),
+            ),
+            SizedBox(width: 24.w),
+          ],
+        ),
+        SizedBox(height: 12.h),
+        Container(
+          height: 2.w,
+          color: Hues.white,
+        ),
+        SizedBox(
+          height: 520.h,
+          width: 1.sw,
+          child: AspectRatio(
+            aspectRatio: controller!.value.aspectRatio,
+            child: CameraPreview(
+              controller!,
+            ),
+          ),
+        ),
+        Container(
+          height: 2.w,
+          color: Hues.white,
+        ),
+        const Expanded(child: SizedBox()),
+        _CameraShutter(
+          key: key,
+          onShutter: onShutter,
+        ),
+        SizedBox(height: 48.h),
+      ],
+    );
+  }
+}
+
+class _CameraBack extends StatelessWidget {
+  final CameraController? controller;
+  final void Function()? onShutter;
+
+  const _CameraBack({
+    super.key,
+    this.controller,
+    this.onShutter,
+  });
+
+  @override
+  Widget build(BuildContext context) {
     return AspectRatio(
       aspectRatio: controller!.value.aspectRatio,
       child: CameraPreview(
